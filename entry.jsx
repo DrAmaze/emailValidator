ca6ba4dc701aa19f2b6065f65e18e5fd923769d4
jacob <jbutler8@terpmail.umd.edu>
2018-02-05 10:58:25 -0800

    Environment setup

---
+++ entry.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Email from './emailForm';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const root = document.getElementById('root');
+  ReactDOM.render(<Email/>, root);
+});
