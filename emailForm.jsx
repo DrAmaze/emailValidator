66356fe0235c55bf13604f67d84b9968ec6840b8
jacob <jbutler8@terpmail.umd.edu>
2018-02-05 11:35:38 -0800

    update component functionality and style

--- emailForm.jsx
+++ emailForm.jsx
@@ -3,13 +3,34 @@ import React from 'react';
 class EmailForm extends React.Component {
   constructor(props) {
     super(props);
+    this.state = { email: '' };
 
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.renderErrors = this.renderErrors.bind(this);
+  }
+
+  update(field) {
+    return e => this.setState({
+      [field] : e.currentTarget.value
+    });
+  }
+
+
+
+  renderErrors() {
+
+  }
+
+  clear(e) {
+    e.preventDefault();
+    this.setState({ email: '' });
   }
 
   render() {
     return (
       <div>
-        <h1>Hello World</h1>
+        <h1>Email Validator</h1>
+
       </div>
     );
   }
