4b09032a1d2aba32fc40f83b344371418fa678e0
jacob <jbutler8@terpmail.umd.edu>
2018-02-05 14:23:32 -0800

    functioning form with adequate styling

--- emailForm.jsx
+++ emailForm.jsx
@@ -3,7 +3,14 @@ import React from 'react';
 class EmailForm extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { email: '' };
+    this.state = {
+      email: '',
+      submitMsg: ''
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.verifyEmail = this.verifyEmail.bind(this);
+    this.validMsg = this.validMsg.bind(this);
   }
 
   update(field) {
@@ -12,20 +19,70 @@ class EmailForm extends React.Component {
     });
   }
 
-  renderErrors() {
+  handleSubmit(e) {
+    e.preventDefault();
+    let valid = this.verifyEmail(this.state.email);
 
+    return this.validMsg(valid);
   }
 
-  clear(e) {
-    e.preventDefault();
-    this.setState({ email: '' });
+  verifyEmail(email) {
+    // A simple check to ensure there is only one '@'
+    let atCount = 0;
+    this.state.email.split("").forEach(letter => {
+      if (letter === '@') {
+        atCount++;
+
+        // optimize solution by breaking and returning false if two '@'
+        // are found in the email address
+        if (atCount > 1) {
+          return false;
+        }
+      }
+    });
+
+    // Regular expression that checks for valid email
+    var pattern = /^([\w\.]*)@[a-zA-Z_]+?\.([a-zA-Z].{2,5}){0,1}$/;
+
+    return pattern.test(this.state.email);
+  }
+
+  validMsg(validEmail) {
+    let email = this.state.email;
+    if (email === '') {
+      this.setState({
+        submitMsg: `Please enter an email address`
+      });
+    } else if (validEmail) {
+      this.setState({
+        submitMsg: `Congratulations! ${email} is a valid email address`
+      });
+    } else {
+      this.setState({
+        submitMsg: `${email} is NOT a valid email address`
+      });
+    }
   }
 
   render() {
     return (
-      <div>
-        <h1>Email Validator</h1>
+      <div className='email'>
+        <form onSubmit={this.handleSubmit}>
+          <h1>Email Validator</h1>
 
+          <input
+            type='text'
+            onChange={this.update('email')}
+            placeholder={'Type email ...'}/>
+          <br/>
+          <input
+            type='submit'
+            value='Verify Email'/>
+        </form>
+
+        <div>
+          {this.state.submitMsg}
+        </div>
       </div>
     );
   }
